ec0cae26ac81ca853e7615abe85eb07785ca592f
Antoniosob42 <antoniosobrinomartin@gmail.com>
2026-09-09 19:14:02 +0200

    (C) cambios en app.js y index.html
    He añadido una id al BotonJugar de index.html, y a el h3 del juego 2, para seleccionarlos en app.js y añadir una alert al pulsar en "Jugar" del Juego 2

--- js/app.js
+++ js/app.js
@@ -5,7 +5,11 @@
 
 document.addEventListener("DOMContentLoaded", () => {
     const startButton = document.querySelector("#startButton");
-
+    const gameName= document.querySelector("#gameName");
+    const playButton = document.querySelector("#BotonJugarJ2");
+    playButton.addEventListener("click",() =>{
+        alert("Vamos a jugar a "+ gameName.textContent)
+    })
     startButton.addEventListener("click", () => {
         alert("¡Bienvenido a GameZone! Comienza la práctica.");
     });
