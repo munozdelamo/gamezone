15dd4309de82b5ab74726e630837aaf79b34609b
jesus baptista <jesusbcdaw2@gmail.com>
2026-09-09 19:15:37 +0200

    mensaje js
    mensaje primer boton

--- js/app.js
+++ js/app.js
@@ -5,8 +5,14 @@
 
 document.addEventListener("DOMContentLoaded", () => {
     const startButton = document.querySelector("#startButton");
+    // jbc lineas 8-9
+    const mensaje = document.querySelector("#mensaje");
 
     startButton.addEventListener("click", () => {
         alert("¡Bienvenido a GameZone! Comienza la práctica.");
+    });
+    // jbc linas 14-17 primer boton
+    mensaje.addEventListener("click", () => {
+        alert("No te vicies.");
     });
 });
